0b7129e54c2014b4dfba454f759152ec8007e75f
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-08-04 21:28:03 +0000

    Deploying to gh-pages from @ GoitStudentsWorks/node_fs99_front_Hellcat232@0c73ed770d9de862a719ca1770511980282bf333 🚀

---
+++ assets/NotFoundPage-BU9JaR9M.js
@@ -0,0 +1,2 @@
+import{j as s,L as o}from"./index-DixpHJSG.js";const n=()=>s.jsxs("div",{children:[s.jsx("h2",{children:"Page not found!"}),s.jsxs(o,{to:"/",children:["Back to homepage",s.jsx("span",{className:"round"})]})]});export{n as default};
+//# sourceMappingURL=NotFoundPage-BU9JaR9M.js.map
